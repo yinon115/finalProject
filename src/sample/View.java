0f67a7c639706bcf260fe2f740679cf51c514a99
Shlomi89 <Shlomis@windowslive.com>
2021-07-29 15:58:23 +0300

    das

--- src/sample/View.java
+++ src/sample/View.java
@@ -40,7 +40,7 @@ public class View extends Application {
         primaryStage.setScene(scene);
         primaryStage.show();
     }
-
+//
 
     public static void main(String[] args) {
         launch(args);
